100caf7f605d541aadb9964992f40ffb6d83eb39
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-10-24 00:59:55 +0300

    Update historySlice.ts

--- src/redux/slices/historySlice.ts
+++ src/redux/slices/historySlice.ts
@@ -1,16 +1,55 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { getFeedsApi } from '@api';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { TOrder } from '@utils-types';
 
-type THistoryState = {};
+type THistoryState = {
+  isLoading: boolean;
+  feeds: {
+    orders: TOrder[];
+    total: number;
+    totalToday: number;
+  };
+};
 
-const initialState: THistoryState = {};
+const initialState: THistoryState = {
+  isLoading: false,
+  feeds: {
+    orders: [],
+    total: 0,
+    totalToday: 0
+  }
+};
 
 const slice = createSlice({
   name: 'history',
   initialState,
   reducers: {},
-  selectors: {}
+  selectors: {
+    getFeeds: (state) => state.feeds,
+    getOrders: (state) => state.feeds.orders,
+    getTotal: (state) => state.feeds.total,
+    getTotalToday: (state) => state.feeds.totalToday
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchFeeds.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchFeeds.rejected, (state) => {
+        state.isLoading = false;
+      })
+      .addCase(fetchFeeds.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.feeds = action.payload;
+      });
+  }
 });
 
+export const fetchFeeds = createAsyncThunk(
+  'feed/getAll',
+  async () => await getFeedsApi()
+);
+
 export const {} = slice.actions;
-export const {} = slice.selectors;
+export const { getOrders, getTotal, getTotalToday, getFeeds } = slice.selectors;
 export default slice.reducer;
