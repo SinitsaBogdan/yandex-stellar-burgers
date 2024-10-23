30fc4bb55279dfc31d0e60729b77bf77b11df3dd
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-10-23 21:18:37 +0300

    Create orderSlice.ts

---
+++ src/redux/slices/orderSlice.ts
@@ -0,0 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+type TOrdersState = {};
+
+const initialState: TOrdersState = {};
+
+const slice = createSlice({
+  name: 'order',
+  initialState,
+  reducers: {},
+  selectors: {}
+});
+
+export const {} = slice.actions;
+export const {} = slice.selectors;
+export default slice.reducer;
