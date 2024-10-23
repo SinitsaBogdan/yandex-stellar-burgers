bd76d222f2344e4363b3577f6f3f96875fad2e55
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-10-23 21:18:35 +0300

    Create ingredientsSlice.ts

---
+++ src/redux/slices/ingredientsSlice.ts
@@ -0,0 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+type TIngredientsState = {};
+
+const initialState: TIngredientsState = {};
+
+const slice = createSlice({
+  name: 'ingredients',
+  initialState,
+  reducers: {},
+  selectors: {}
+});
+
+export const {} = slice.actions;
+export const {} = slice.selectors;
+export default slice.reducer;
