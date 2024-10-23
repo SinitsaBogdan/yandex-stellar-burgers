44fee02df512f294cc07b1c570c069cf7a2def71
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-10-24 00:59:54 +0300

    Update feed.tsx

--- src/pages/feed/feed.tsx
+++ src/pages/feed/feed.tsx
@@ -2,14 +2,23 @@ import { Preloader } from '@ui';
 import { FeedUI } from '@ui-pages';
 import { TOrder } from '@utils-types';
 import { FC } from 'react';
+import { fetchFeeds, getFeedsOrders } from '../../redux/slices/historySlice';
+import { useDispatch, useSelector } from '../../redux/store';
 
 export const Feed: FC = () => {
-  /** TODO: взять переменную из стора */
-  const orders: TOrder[] = [];
+  const orders: TOrder[] = useSelector(getFeedsOrders);
+  const dispatch = useDispatch();
 
   if (!orders.length) {
     return <Preloader />;
   }
 
-  <FeedUI orders={orders} handleGetFeeds={() => {}} />;
+  return (
+    <FeedUI
+      orders={orders}
+      handleGetFeeds={() => {
+        dispatch(fetchFeeds());
+      }}
+    />
+  );
 };
