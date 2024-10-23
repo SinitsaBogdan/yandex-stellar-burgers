50e0d7ce6dd73c7da0ef25d286c5428d53e4cc75
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-10-23 21:18:32 +0300

    Create historySlice.ts

---
+++ src/redux/slices/historySlice.ts
@@ -0,0 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+type THistoryState = {};
+
+const initialState: THistoryState = {};
+
+const slice = createSlice({
+  name: 'history',
+  initialState,
+  reducers: {},
+  selectors: {}
+});
+
+export const {} = slice.actions;
+export const {} = slice.selectors;
+export default slice.reducer;
