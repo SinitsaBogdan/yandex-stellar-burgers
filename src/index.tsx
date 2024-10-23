2435b33b93b0857965086444c338089daee37b07
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-10-23 21:26:25 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,16 @@ import * as ReactDOMClient from 'react-dom/client';
 import App from './components/app/app';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { BrowserRouter } from 'react-router-dom';
 const container = document.getElementById('root') as HTMLElement;
 const root = ReactDOMClient.createRoot(container!);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
