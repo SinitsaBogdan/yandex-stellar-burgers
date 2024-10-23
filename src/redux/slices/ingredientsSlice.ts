122acb7a9e58ac10d4c4931058c276a894cd02e9
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-10-23 23:19:07 +0300

    Update ingredientsSlice.ts

--- src/redux/slices/ingredientsSlice.ts
+++ src/redux/slices/ingredientsSlice.ts
@@ -1,16 +1,48 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { getIngredientsApi } from '@api';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { TIngredient } from '@utils-types';
 
-type TIngredientsState = {};
+type TIngredientsState = {
+  isLoading: boolean;
+  ingredients: TIngredient[];
+};
 
-const initialState: TIngredientsState = {};
+const initialState: TIngredientsState = {
+  isLoading: false,
+  ingredients: []
+};
 
 const slice = createSlice({
   name: 'ingredients',
   initialState,
-  reducers: {},
-  selectors: {}
+  reducers: {
+    setIngredients: (state, action) => {
+      state.ingredients = action.payload;
+    }
+  },
+  selectors: {
+    getIngredients: (state) => state.ingredients
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchIngredients.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchIngredients.rejected, (state) => {
+        state.isLoading = false;
+      })
+      .addCase(fetchIngredients.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.ingredients = action.payload;
+      });
+  }
 });
 
-export const {} = slice.actions;
-export const {} = slice.selectors;
+export const fetchIngredients = createAsyncThunk(
+  'ingredients/getAll',
+  async () => getIngredientsApi()
+);
+
+export const { setIngredients } = slice.actions;
+export const { getIngredients } = slice.selectors;
 export default slice.reducer;
