b585f65756d4bfe259a323fea626f1ed90ae1b82
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-10-24 19:35:33 +0300

    Update order-info.tsx

--- src/components/order-info/order-info.tsx
+++ src/components/order-info/order-info.tsx
@@ -1,21 +1,22 @@
-import { FC, useMemo } from 'react';
+import { FC, useEffect, useMemo } from 'react';
 import { Preloader } from '../ui/preloader';
 import { OrderInfoUI } from '../ui/order-info';
 import { TIngredient } from '@utils-types';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from '../../redux/store';
+import { fetchOrderId, getOrderData } from '../../redux/slices/orderSlice';
+import { getIngredients } from '../../redux/slices/ingredientsSlice';
 
 export const OrderInfo: FC = () => {
-  /** TODO: взять переменные orderData и ingredients из стора */
-  const orderData = {
-    createdAt: '',
-    ingredients: [],
-    _id: '',
-    status: '',
-    name: '',
-    updatedAt: 'string',
-    number: 0
-  };
+  const { number } = useParams();
+  const dispatch = useDispatch();
+  const orderData = useSelector(getOrderData);
+  console.log(orderData);
+  useEffect(() => {
+    if (number) dispatch(fetchOrderId(number));
+  }, []);
 
-  const ingredients: TIngredient[] = [];
+  const ingredients: TIngredient[] = useSelector(getIngredients);
 
   /* Готовим данные для отображения */
   const orderInfo = useMemo(() => {
