6b2d3d8d66fc63e0aef5e6dbe807cc180f417579
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-10-24 19:35:35 +0300

    Update orderSlice.ts

--- src/redux/slices/orderSlice.ts
+++ src/redux/slices/orderSlice.ts
@@ -1,16 +1,45 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { getOrderByNumberApi } from '@api';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { TOrder } from '@utils-types';
 
-type TOrdersState = {};
+type TOrdersState = {
+  isLoading: boolean;
+  orderData: TOrder | null;
+};
 
-const initialState: TOrdersState = {};
+const initialState: TOrdersState = {
+  isLoading: false,
+  orderData: null
+};
 
 const slice = createSlice({
   name: 'order',
   initialState,
   reducers: {},
-  selectors: {}
+  selectors: {
+    getIsLoading: (state) => state.isLoading,
+    getOrderData: (state) => state.orderData
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchOrderId.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchOrderId.rejected, (state) => {
+        state.isLoading = false;
+      })
+      .addCase(fetchOrderId.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.orderData = action.payload.orders[0];
+      });
+  }
 });
 
+export const fetchOrderId = createAsyncThunk(
+  'feed/getOrderId',
+  async (number: string) => await getOrderByNumberApi(Number(number))
+);
+
 export const {} = slice.actions;
-export const {} = slice.selectors;
+export const { getIsLoading, getOrderData } = slice.selectors;
 export default slice.reducer;
