850cc47e8f6c5994a9e2eafee6f5fcdfacc69950
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2024-10-29 12:25:40 +0300

    Update feed.tsx

--- src/pages/feed/feed.tsx
+++ src/pages/feed/feed.tsx
@@ -1,13 +1,17 @@
 import { Preloader } from '@ui';
 import { FeedUI } from '@ui-pages';
 import { TOrder } from '@utils-types';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { fetchFeeds, getFeedsOrders } from '../../redux/slices/historySlice';
 import { useDispatch, useSelector } from '../../redux/store';
 
 export const Feed: FC = () => {
   const orders: TOrder[] = useSelector(getFeedsOrders);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchFeeds());
+  }, [dispatch]);
 
   if (!orders.length) {
     return <Preloader />;
