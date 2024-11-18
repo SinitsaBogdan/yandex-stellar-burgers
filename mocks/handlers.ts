b2cdf90a6353b216f516a5bd61eab86cfc567e29
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2024-11-18 22:59:30 +0300

    mocks

---
+++ mocks/handlers.ts
@@ -0,0 +1,12 @@
+import {
+    http,
+    HttpResponse
+} from 'msw'
+
+// export const handlers = [
+//     http.get('http://localhost:4000/', () => {
+//         return HttpResponse.json({
+//             status: true
+//         });
+//     })
+// ];
