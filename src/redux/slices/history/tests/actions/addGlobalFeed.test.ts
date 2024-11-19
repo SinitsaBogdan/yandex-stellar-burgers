c11e73d7bc6dd93138a156a463f462268f10558c
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2024-11-19 23:26:42 +0300

    Update addGlobalFeed.test.ts

--- src/redux/slices/history/tests/actions/addGlobalFeed.test.ts
+++ src/redux/slices/history/tests/actions/addGlobalFeed.test.ts
@@ -1,5 +1,11 @@
-import { test, describe } from '@jest/globals';
+import { test } from '@jest/globals';
+import slice, { initialState, addGlobalFeed } from '../../historySlice';
+import response from "../feedsResponse.json";
 
-describe('[ addGlobalFeed ] - .', () => {
-  test('', () => {});
+const initial = JSON.parse(JSON.stringify(initialState)) as typeof initialState;
+
+test('[ addGlobalFeed ] - Проверка редюссера.', () => {
+  const newState = slice(initial, addGlobalFeed(response));
+  const { feeds } = newState;
+  expect(feeds).toEqual(response);
 });
