4707509baa7105372187af1556fa04b33b2b0c7f
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2024-11-19 23:26:54 +0300

    Update clearOrderData.test.ts

--- src/redux/slices/orders/tests/actions/clearOrderData.test.ts
+++ src/redux/slices/orders/tests/actions/clearOrderData.test.ts
@@ -1,5 +1,12 @@
-import { test, describe } from '@jest/globals';
+import { test } from '@jest/globals';
+import slice, { clearOrderData, initialState } from '../../orderSlice';
+import data from "../order.json";
 
-describe('[ clearOrderData ] - .', () => {
-  test('', () => {});
+const initial = JSON.parse(JSON.stringify(initialState)) as typeof initialState;
+initial.orderData = data;
+
+test('[ clearOrderData ] - Проверка редюссера.', () => {
+  const newState = slice(initial, clearOrderData());
+  const { orderData } = newState;
+  expect(orderData).toBeNull();
 });
