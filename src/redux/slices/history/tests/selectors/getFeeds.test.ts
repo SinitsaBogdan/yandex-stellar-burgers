c842a10cd09ae01f49547fc565389e3ae35d0891
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2024-11-19 21:28:15 +0300

    Update getFeeds.test.ts

--- src/redux/slices/history/tests/selectors/getFeeds.test.ts
+++ src/redux/slices/history/tests/selectors/getFeeds.test.ts
@@ -1,8 +1,20 @@
-import { test, describe } from '@jest/globals';
+import { test } from '@jest/globals';
+import { configureStore } from '@reduxjs/toolkit';
+import slice from '../../historySlice';
+import { getFeeds } from '../../historySlice';
 
-describe('[ getFeeds ] - Проверка селлектора.', () => {
+test('[ getFeeds ] - Проверка селлектора.', () => {
+  const store = configureStore({
+    reducer: {
+      history: slice
+    }
+  });
 
-    test('', () => {
+  const response = getFeeds(store.getState());
 
-    })
-})
+  expect(response).toEqual({
+    orders: [],
+    total: 0,
+    totalToday: 0
+  });
+});
