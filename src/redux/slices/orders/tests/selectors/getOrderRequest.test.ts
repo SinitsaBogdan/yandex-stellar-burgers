2d8a431e6d41aa686ad4d61a805c9bb78e5a3f17
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2024-11-19 21:28:36 +0300

    Update getOrderRequest.test.ts

--- src/redux/slices/orders/tests/selectors/getOrderRequest.test.ts
+++ src/redux/slices/orders/tests/selectors/getOrderRequest.test.ts
@@ -1,16 +1,16 @@
 import { test } from '@jest/globals';
 import { configureStore } from '@reduxjs/toolkit';
-import orderSlice from "../../orderSlice";
-import { getOrderRequest } from "../../orderSlice";
+import slice from '../../orderSlice';
+import { getOrderRequest } from '../../orderSlice';
 
 test('[ getOrderRequest ] - Проверка селлектора.', () => {
-    const store = configureStore({
-        reducer: {
-            order: orderSlice
-        },
-    });
+  const store = configureStore({
+    reducer: {
+      order: slice
+    }
+  });
 
-    const response = getOrderRequest(store.getState());
+  const response = getOrderRequest(store.getState());
 
-    expect(response).toEqual(false)
-})
+  expect(response).toEqual(false);
+});
