541ba56f81400dc36025ba7240f8fe2e6ea3cb57
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2024-11-19 21:28:09 +0300

    Update getIngredients.test.ts

--- src/redux/slices/constructors/tests/selectors/getIngredients.test.ts
+++ src/redux/slices/constructors/tests/selectors/getIngredients.test.ts
@@ -1,8 +1,16 @@
-import { test, describe } from '@jest/globals';
+import { test } from '@jest/globals';
+import { configureStore } from '@reduxjs/toolkit';
+import slice from '../../constructorItemSlice';
+import { getIngredients } from '../../constructorItemSlice';
 
-describe('[ getIngredients ] - Проверка селлектора.', () => {
+test('[ getIngredients ] - Проверка селлектора.', () => {
+  const store = configureStore({
+    reducer: {
+      item: slice
+    }
+  });
 
-    test('', () => {
+  const response = getIngredients(store.getState());
 
-    })
-})
+  expect(response).toEqual([]);
+});
