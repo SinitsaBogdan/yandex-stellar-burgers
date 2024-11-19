0804bdc53079911704b54acf1d3f2e57d878b725
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2024-11-19 21:28:43 +0300

    Update fetchUserCheck.test.ts

--- src/redux/slices/users/tests/actions/fetchUserCheck.test.ts
+++ src/redux/slices/users/tests/actions/fetchUserCheck.test.ts
@@ -1,6 +1,25 @@
-import { test, describe } from '@jest/globals';
+import { test } from '@jest/globals';
+import { configureStore } from '@reduxjs/toolkit';
+import slice from '../../userSlice';
+import { fetchUserCheck } from '../../thunks';
 
-describe('[ setIngredient ] - .', () => {
-    test('', () => {
+test('[ fetchUserCheck ] - Асинхронное событие.', async () => {
+  const expected = {
+    email: 'admin@yandex.ru',
+    name: 'admin'
+  };
+
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      json: () => Promise.resolve(expected)
     })
-})
+  ) as jest.Mock;
+
+  const store = configureStore({
+    reducer: { user: slice }
+  });
+
+  await store.dispatch(fetchUserCheck());
+  const { user } = store.getState().user;
+  expect(user).toEqual(expected)
+});
