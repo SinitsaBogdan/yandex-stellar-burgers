5a39a8dada67566ca0b752da17c4e9d876256f0b
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2024-11-19 21:28:26 +0300

    Update thunks.ts

--- src/redux/slices/ingredients/thunks.ts
+++ src/redux/slices/ingredients/thunks.ts
@@ -1,5 +1,5 @@
-import { getIngredientsApi } from "../../../utils/burger-api";
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import { getIngredientsApi } from '../../../utils/burger-api';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
 export const fetchIngredients = createAsyncThunk(
   'ingredients/getAll',
