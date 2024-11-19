6ec196a7f57af8b8eb3cd5b15be44957e4d420fb
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2024-11-19 21:28:33 +0300

    Update getIsLoading.test.ts

--- src/redux/slices/orders/tests/selectors/getIsLoading.test.ts
+++ src/redux/slices/orders/tests/selectors/getIsLoading.test.ts
@@ -1,16 +1,16 @@
 import { test } from '@jest/globals';
 import { configureStore } from '@reduxjs/toolkit';
-import orderSlice from "../../orderSlice";
-import { getIsLoading } from "../../orderSlice";
+import slice from '../../orderSlice';
+import { getIsLoading } from '../../orderSlice';
 
 test('[ getIsLoading ] - Проверка селлектора.', () => {
-    const store = configureStore({
-        reducer: {
-            order: orderSlice
-        },
-    });
+  const store = configureStore({
+    reducer: {
+      order: slice
+    }
+  });
 
-    const response = getIsLoading(store.getState());
+  const response = getIsLoading(store.getState());
 
-    expect(response).toEqual(false)
-})
+  expect(response).toEqual(false);
+});
