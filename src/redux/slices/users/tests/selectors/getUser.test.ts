2986337c5e14631070812215d34adbedb92bcd0b
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2024-11-19 21:30:31 +0300

    Update getUser.test.ts

--- src/redux/slices/users/tests/selectors/getUser.test.ts
+++ src/redux/slices/users/tests/selectors/getUser.test.ts
@@ -1,21 +1,19 @@
 import { test, describe } from '@jest/globals';
 import { configureStore } from '@reduxjs/toolkit';
-import userSlice from "../../userSlice";
-import { selectUser } from "../../userSlice";
+import slice from '../../userSlice';
+import { selectUser } from '../../userSlice';
 
 test('[ getUser ] - Проверка селлектора.', () => {
-    const store = configureStore({
-        reducer: {
-            user: userSlice
-        },
-    });
+  const store = configureStore({
+    reducer: {
+      user: slice
+    }
+  });
 
-    const response = selectUser(store.getState());
+  const response = selectUser(store.getState());
 
-    expect(response).toEqual(
-        {
-            email: '',
-            name: ''
-        }
-    )
-})
+  expect(response).toEqual({
+    email: '',
+    name: ''
+  });
+});
