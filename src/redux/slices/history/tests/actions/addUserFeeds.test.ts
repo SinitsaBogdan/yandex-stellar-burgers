4fa887c8eb3ebf50a32a256a59e999969af694ac
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2024-11-19 23:26:44 +0300

    Update addUserFeeds.test.ts

--- src/redux/slices/history/tests/actions/addUserFeeds.test.ts
+++ src/redux/slices/history/tests/actions/addUserFeeds.test.ts
@@ -1,5 +1,11 @@
-import { test, describe } from '@jest/globals';
+import { test } from '@jest/globals';
+import slice, { initialState, addUserFeeds } from '../../historySlice';
+import response from "../order.json";
 
-describe('[ addUserFeeds ] - .', () => {
-  test('', () => {});
+const initial = JSON.parse(JSON.stringify(initialState)) as typeof initialState;
+
+test('[ addUserFeeds ] - Проверка редюссера.', () => {
+  const newState = slice(initial, addUserFeeds([response]));
+  const { history } = newState;
+  expect(history).toEqual([response]);
 });
