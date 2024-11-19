e1f2ed56dfe851039612ec605a012478f4138a74
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2024-11-19 21:28:37 +0300

    Update thunks.ts

--- src/redux/slices/orders/thunks.ts
+++ src/redux/slices/orders/thunks.ts
@@ -1,5 +1,5 @@
-import { getOrderByNumberApi, orderBurgerApi } from "../../../utils/burger-api";
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import { getOrderByNumberApi, orderBurgerApi } from '../../../utils/burger-api';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
 export const fetchOrderId = createAsyncThunk(
   'order/getId',
