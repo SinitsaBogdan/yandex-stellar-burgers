9a22aab7c5e718f0c5e4b92ac79112a857329655
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2024-11-19 23:26:32 +0300

    Update clear.test.ts

--- src/redux/slices/constructors/tests/actions/clear.test.ts
+++ src/redux/slices/constructors/tests/actions/clear.test.ts
@@ -1,5 +1,13 @@
-import { test, describe } from '@jest/globals';
+import { test } from '@jest/globals';
+import slice, { initialState } from '../../constructorItemSlice';
+import { clear } from '../../constructorItemSlice';
+import data from '../ingridients.json';
 
-describe('[ clear ] - .', () => {
-  test('', () => {});
+const initial = JSON.parse(JSON.stringify(initialState)) as typeof initialState;
+initial.ingredients = data;
+
+test('[ clear ] - Проверка редюссера.', () => {
+  const newState = slice(initial, clear());
+  const { ingredients } = newState;
+  expect(ingredients).toEqual([]);
 });
