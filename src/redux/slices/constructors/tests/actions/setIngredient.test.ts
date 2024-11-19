ce4e304c4a7773305a28ecb6f5c48fcab038876e
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2024-11-19 23:26:35 +0300

    Update setIngredient.test.ts

--- src/redux/slices/constructors/tests/actions/setIngredient.test.ts
+++ src/redux/slices/constructors/tests/actions/setIngredient.test.ts
@@ -1,5 +1,13 @@
-import { test, describe } from '@jest/globals';
+import { test } from '@jest/globals';
+import slice, { initialState, setIngredient } from '../../constructorItemSlice';
+import data from '../ingridients.json';
+import newItem from '../newIngridient.json';
 
-describe('[ setIngredient ] - .', () => {
-  test('', () => {});
+const initial = JSON.parse(JSON.stringify(initialState)) as typeof initialState;
+initial.ingredients = data;
+
+test('[ setIngredient ] - Проверка редюссера.', () => {
+  const newState = slice(initial, setIngredient(newItem));
+  const { ingredients } = newState;
+  expect(ingredients).toEqual([...data, newItem]);
 });
