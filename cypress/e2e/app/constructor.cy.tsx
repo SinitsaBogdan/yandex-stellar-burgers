824a2e7d4fd39d611821d9d689a141739d71e500
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2024-11-20 00:28:22 +0300

    Delete constructor.cy.tsx

--- cypress/e2e/app/constructor.cy.tsx
+++
@@ -1,5 +0,0 @@
-describe('проверяем доступность приложения', function() {
-    it('Приложение должно быть доступно по адресу localhost:4000', function() {
-        cy.visit('http://localhost:4000');
-    });
-});
