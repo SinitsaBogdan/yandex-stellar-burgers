a27b14babcb7df8629eae102157d1d2741a6c0d7
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2024-11-19 23:26:34 +0300

    Update setBun.test.ts

--- src/redux/slices/constructors/tests/actions/setBun.test.ts
+++ src/redux/slices/constructors/tests/actions/setBun.test.ts
@@ -1,5 +1,12 @@
-import { test, describe } from '@jest/globals';
+import { test } from '@jest/globals';
+import slice, { initialState } from '../../constructorItemSlice';
+import { setBun } from '../../constructorItemSlice';
+import newItem from '../newIngridient.json';
 
-describe('[ setBun ] - .', () => {
-  test('', () => {});
+const initial = JSON.parse(JSON.stringify(initialState)) as typeof initialState;
+
+test('[ setBun ] - Проверка редюссера.', () => {
+  const newState = slice(initial, setBun(newItem));
+  const { bun } = newState;
+  expect(bun).toEqual(newItem);
 });
