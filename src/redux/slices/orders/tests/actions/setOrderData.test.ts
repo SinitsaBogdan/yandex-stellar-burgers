86ba8319820ad9fb9313a1266123e3f2fc4d52d8
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2024-11-19 23:26:56 +0300

    Update setOrderData.test.ts

--- src/redux/slices/orders/tests/actions/setOrderData.test.ts
+++ src/redux/slices/orders/tests/actions/setOrderData.test.ts
@@ -1,5 +1,11 @@
-import { test, describe } from '@jest/globals';
+import { test } from '@jest/globals';
+import data from "../order.json";
+import slice, { initialState, setOrderData } from '../../orderSlice';
 
-describe('[ setOrderData ] - .', () => {
-  test('', () => {});
+const initial = JSON.parse(JSON.stringify(initialState)) as typeof initialState;
+
+test('[ setOrderData ] - Проверка редюссера.', () => {
+  const newState = slice(initial, setOrderData(data));
+  const { orderData } = newState;
+  expect(orderData).not.toBeNull();
 });
